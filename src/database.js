a30ce6cc9057cf894ada0d79234b0f87529e1fc3
CarlosD95 <carlosdaserrano4@gmail.com>
2023-10-30 18:38:32 -0400

    Changes in index.hbs and about.hbs

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE   } = process.env
 const MONGODB_URL = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true, 
 })
